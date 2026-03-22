ca2435c1f680c87c23f4803efa1664fce39c4b79
ciprianborza <73166666+ciprianborza@users.noreply.github.com>
2026-03-22 17:51:35 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://qmjjrbtjcmthxzwjhfwg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtampyYnRqY210aHh6d2poZndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODcwNDEsImV4cCI6MjA4OTc2MzA0MX0.lG-xr_fv0TkHCxRrKDgOYbvYd1QtJgRh6Di-cs0ggZg';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
